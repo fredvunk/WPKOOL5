f7258ee08a486218ba133269cb129719a5a2d4b0
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-16 10:22:12 +0200

    modified plugin `testplugin` version 1.0 (after activation of `testplugin` version 1.0)

---
+++ wp-content/plugins/plugintest/testplugin.php
@@ -0,0 +1,7 @@
+<?php
+ /* Plugin name: testplugin
+ Plugin URL: http://fredvunk.ikt.khk.ee/wordpress5/
+ Description: This is the very first plugin I ever created.
+ Version: 1.0
+ Author: Fred Vunk
+ Author URL: http://fredvunk.ikt.khk.ee/wordpress5/ */
