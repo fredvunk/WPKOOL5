94ffbb82b265eb8cfd4d29c859f32edc44311106
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-16 10:25:41 +0200

    modified plugin `testplugin` version 1.0 (after activation of `testplugin` version 1.0)

---
+++ wp-content/plugins/plugintest/testplugin.php
@@ -0,0 +1,13 @@
+<?php
+ /* Plugin name: testplugin
+ Plugin URL: http://fredvunk.ikt.khk.ee/wordpress5/
+ Description: This is the very first plugin I ever created.
+ Version: 1.0
+ Author: Fred Vunk
+ Author URL: http://fredvunk.ikt.khk.ee/wordpress5/ */
+ 
+this.snowColor = ‘#f00’; // Use for hearts color
+
+var symbls = new Array(‘♥’,’&#127801’, &#127852); // falling hearts and tunes array
+
+var sclrs = new Array(‘#ff0000′,’#2CAA47′,’#E8EF1F’,’#0080FF’,’#ffffff’); // colors array
