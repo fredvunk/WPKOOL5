ec5339d5d9bc3902c988631301093d3a5958f7fb
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-16 10:24:07 +0200

    deleted plugin `testplugin` version 1.0

--- wp-content/plugins/plugintest/testplugin.php
+++
@@ -1,7 +0,0 @@
-<?php
- /* Plugin name: testplugin
- Plugin URL: http://fredvunk.ikt.khk.ee/wordpress5/
- Description: This is the very first plugin I ever created.
- Version: 1.0
- Author: Fred Vunk
- Author URL: http://fredvunk.ikt.khk.ee/wordpress5/ */
