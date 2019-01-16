64e3200662ea9fc714f67b647c3ecfe9fbb688eb
fredvunkiktkhk <fred.vunk@khk.ee>
2019-01-16 11:27:28 +0200

    modified plugin `valentines-hearts.php`

--- wp-content/plugins/valentines-day-floating-hearts/includes/valentines-hearts.php
+++ wp-content/plugins/valentines-day-floating-hearts/includes/valentines-hearts.php
@@ -26,15 +26,15 @@ function dc_valentines_heart() {
 	// Set the letter that creates your heartflake (recommended: * )
 	var heartletter="<i class='dcfa dcfa-heart' style='color:"+heartcolor[randommaker(heartcolor.length)]+" !important;' ></i>"
 	// Set the speed of sinking (recommended values range from 0.3 to 2)
-	var sinkspeed=0.6
+	var sinkspeed=0.4
 	// Set the maximum-size of your heartflakes
-	var heartmaxsize=30
+	var heartmaxsize=60
 	// Set the minimal-size of your heartflakes
 	var heartminsize=8
 	// Set the hearting-zone
 	// Set 1 for all-over-hearting, set 2 for left-side-hearting
 	// Set 3 for center-hearting, set 4 for right-side-hearting
-	var heartingzone=1
+	var heartingzone=(2,3)
 	///////////////////////////////////////////////////////////////////////////
 	var heart=new Array()
 	var marginbottom
